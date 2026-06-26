16236a8bfa9c96992d81807ce69399bda9e89c72
Maike de Oliveira Nascimento <maikoy606@hotmail.com>
2026-06-26 09:51:24 -0300

    Arquivos feitos ate o momento

---
+++ verilog/counter8b.sv
@@ -0,0 +1,18 @@
+module counter8b(clk, en, sw, outdata);
+	input clk, en, sw;
+	
+	output [7:0] outdata;
+	
+	
+	
+	dff d0();
+	dff d1();
+	dff d2();
+	dff d3();
+	dff d4();
+	dff d5();
+	dff d6();
+	dff d7();
+
+
+endmodule
